5de8a1f97ccce147879f6546faa78cc98ab6ec32
sne-syn <snezhana.syngur@gmail.com>
2019-06-06 13:25:05 +0200

    Начинаем следить за репозиторием

---
+++ course.js
@@ -0,0 +1,35 @@
+"use strict";
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone.name);
+console.log(persone["name"]);  
+
+let arr = ["plum.png", "orange.jpg", "apple.bmp"];
+
+console.log(arr["0"]);
+
+// alert("Hola!");
+
+// let answer = confirm("Are you here?");
+// console.log(answer);
+
+
+// let answer = +prompt("Есть ли вам 18?", "Да");
+
+// console.log(typeof(answer));
+
+// console.log("arr" + " - object");
+// console.log(4 + " - object");
+
+let incr = 10,
+    decr = 10;
+
+console.log(++incr);
+console.log(decr--);
+
+console.log(typeof("2" == 2));
